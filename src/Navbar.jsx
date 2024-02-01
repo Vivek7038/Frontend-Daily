c8726a6122393260b56e5fb27382c6b3638a68f4
Vivek7038 <vivek888chavan@gmail.com>
2024-02-01 09:22:38 +0530

    added github link

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { FaGithub } from "react-icons/fa";
 const Navbar = () => {
-  const navigate=useNavigate();
-  
-  return (
-    <div className='ml-2 mt-2 mb-[2rem] font-bold text-md md:text-lg lg:text-4xl cursor-pointer'
-    onClick={()=>navigate("/")}
-    >Frontend Daily</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Navbar
+  return (
+    <div className="flex flex-row gap-x-3 items-center ml-2 mt-2 mb-[2rem]  ">
+      <div
+        className="font-bold text-md md:text-lg lg:text-4xl cursor-pointer m-0"
+        onClick={() => navigate("/")}
+      >
+        Frontend Daily
+      </div>
+   <div>
+       <Link
+        to="https://github.com/Vivek7038/Frontend-Daily" 
+        target="_blank"
+        >
+        <FaGithub className="w-6 h-6 self-center" />
+      </Link>
+        </div>
+    </div>
+  );
+};
+
+export default Navbar;
